f2e51d7e954b1a53df1a622c417c7da6952dde2b
Zviad Jolokhava <Zviadjolokhava@gmail.com>
2024-04-28 02:30:41 +0200

    third commit

--- installed_packages.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install docker-compose
-sudo apt install gcc-12 virtualbox-7.0
-sudo apt install default-jre
-sudo apt install default-jre
-sudo apt install openjdk-17-jdk
---
+++ instals
@@ -0,0 +1,5 @@
+sudo apt install docker-compose
+sudo apt install gcc-12 virtualbox-7.0
+sudo apt install default-jre
+sudo apt install default-jre
+sudo apt install openjdk-17-jdk
